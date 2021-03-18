f6f401c74e084156b81f6af7a47d24b27d074dea
Maria Shmeleva <smelevamaria1@gmail.com>
2021-03-18 20:15:44 +0300

    1

--- src/com/company/BoxUtil.java
+++ src/com/company/BoxUtil.java
@@ -1,22 +1,22 @@
 package com.company;
 
 public class BoxUtil {
-    public static <T> void copyFromBoxToBox(final Box<T> src, final Box<? super T> dest) {
+    public static <T> void copyFromBoxToBox(Box<T> src, Box<? super T> dest) {
         dest.put(src.get());
     }
 
-    public static <T extends Fruit> void copyFreshFruitFromBoxToBox(final Box<T> src, final Box<Fruit> dest) {
+    public static <T extends Fruit> void copyFreshFruitFromBoxToBox( Box<T> src, Box<Fruit> dest) {
         if (src.get().getFresh()) {
             dest.put(src.get());
         }
     }
 
-    public static <T> void printElementFromTwoBoxes(final Box<Box<T>> box) {
+    public static <T> void printElementFromTwoBoxes(Box<Box<T>> box) {
         Box<T> newBox = box.get();
         System.out.println(newBox.get().toString());
     }
 
-    public static void printElementFromBoxes(final Box<?> box) {
+    public static void printElementFromBoxes(Box<?> box) {
         Object innerObject = box.get();
         while (innerObject instanceof Box) {
             innerObject = ((Box<?>) innerObject).get();
